f5da120dd25212232febd8724490de1762599dfe
Florian Best <best@univention.de>
2016-01-07 18:37:15 +0100

    use library calls

--- conffiles/etc/simplesamlphp/metadata/97saml20-idp-hosted.php
+++ conffiles/etc/simplesamlphp/metadata/97saml20-idp-hosted.php
@@ -1,9 +1,8 @@
 @!@
-enabled = configRegistry.get('privacyidea/saml/enable')
 entity_id = configRegistry.get('saml/idp/entityID',
 		'https://%(hostname)s.%(domainname)s/simplesamlphp/saml2/idp/metadata.php'
 		% configRegistry)
-if enabled.lower() == "true":
+if configRegistry.is_true('privacyidea/saml/enable'):
 	print "$metadata['%s']['auth'] = 'privacyidea';" % (entity_id,)
 @!@
 
