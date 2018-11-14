cb27fee2d1fb52a1138afe4f5be11f88a8935bdd
Micha Preußer <mail@mpreusser.de>
2018-11-14 16:45:00 +0100

    Added authprocfilter to UCS

--- conffiles/etc/simplesamlphp/metadata/97saml20-idp-hosted.php
+++ conffiles/etc/simplesamlphp/metadata/97saml20-idp-hosted.php
@@ -2,7 +2,31 @@
 entity_id = configRegistry.get('saml/idp/entityID',
 		'https://%(hostname)s.%(domainname)s/simplesamlphp/saml2/idp/metadata.php'
 		% configRegistry)
-if configRegistry.is_true('privacyidea/saml/enable'):
+
+hostname = baseConfig.get('hostname')
+domainname = baseConfig.get('domainname')
+url = baseConfig.get('privacyidea/saml/url', 'https://%s.%s/privacyidea' % (hostname, domainname))
+realm = baseConfig.get('privacyidea/saml/realm', '')
+verifyhost = baseConfig.get('privacyidea/saml/verifyhost', 'True')
+verifypeer = baseConfig.get('privacyidea/saml/verifypeer', 'True')
+uid = baseConfig.get('privacyidea/saml/uidkey', 'uid')
+enabled = baseConfig.get('privacyidea/saml/enable')
+
+if enabled == 'authsource' or enabled == 'true':
 	print "$metadata['%s']['auth'] = 'privacyidea';" % (entity_id,)
+elif enabled == 'authproc':
+	print """
+	'authproc' => array(
+		25 => array(
+			'class' => 'privacyidea:privacyidea',
+			'privacyideaserver' => '%s',
+			'realm' => '%s',
+			'uidKey' => '%s',
+			'sslverifyhost' => '%s',
+			'sslverifypeer' => '%s',
+		),
+	),
+	""" % (url, realm, uid, verifyhost, verifypeer)
 @!@
 
+baseConfig.get('privacyidea/saml/verifyhost',
