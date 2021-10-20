2f50a2bb30d55cd03defd692118d3afeabcc4053
pablo-knight <62021672+pablo-knight@users.noreply.github.com>
2021-10-20 15:45:51 +0200

    Update 97saml20-idp-hosted.php

--- conffiles-v1.9/etc/simplesamlphp/metadata/97saml20-idp-hosted.php
+++ conffiles-v1.9/etc/simplesamlphp/metadata/97saml20-idp-hosted.php
@@ -2,7 +2,7 @@
 hostname = configRegistry.get('hostname')
 domainname = configRegistry.get('domainname')
 entity_id = configRegistry.get('saml/idp/entityID',
-		'https://{0!s}.{1!s}/simplesamlphp/saml2/idp/metadata.php'.format(hostname, domainname))
+    'https://{0!s}.{1!s}/simplesamlphp/saml2/idp/metadata.php'.format(hostname, domainname))
 
 url = configRegistry.get('privacyidea/saml/url', 'https://privacyidea')
 verifyhost = configRegistry.get('privacyidea/saml/verifyhost', 'true')
@@ -23,35 +23,32 @@ uidKey = configRegistry.get('privacyidea/saml/uidkey', 'uid')
 enabled = configRegistry.get('privacyidea/saml/enable', 'false')
 
 if enabled == 'authsource' or configRegistry.is_true('privacyidea/saml/enabled'):
-	print("$metadata['{entity_id}']['auth'] = 'privacyidea';".format(entity_id))
+    print("$metadata['{entity_id}']['auth'] = 'privacyidea';".format(entity_id=entity_id))
 elif enabled == 'authproc':
-	print("""
-    $metadata['{entity_id}']['authproc'] = array(
-    """.format(entity_id=entity_id))
+    print("$metadata['{entity_id}']['authproc'] = array(".format(entity_id=entity_id))
 
-	print("""
-		25 => array(
-			'class' => 'privacyidea:PrivacyideaAuthProc',
-			'privacyideaServerURL' => '{url}',
-			'realm' => '{realm}',
-			'uidKey' => '{uidKey}',
-			'sslVerifyHost' => '{verifyhost}',
-			'sslVerifyPeer' => '{verifypeer}',
-	""".format(url=url, realm=realm, uidKey=uidKey, verifyhost=verifyhost.lower(), verifypeer=verifypeer.lower()))
+    print("""
+    25 => array(
+        'class' => 'privacyidea:PrivacyideaAuthProc',
+        'privacyideaServerURL' => '{url}',
+        'realm' => '{realm}',
+        'uidKey' => '{uidKey}',
+        'sslVerifyHost' => '{verifyhost}',
+        'sslVerifyPeer' => '{verifypeer}',
+        """.format(url=url, realm=realm, uidKey=uidKey, verifyhost=verifyhost.lower(), verifypeer=verifypeer.lower()))
 
-if excludeClientIPs != '':
+    if excludeClientIPs != '':
         print("""
-            'excludeClientIPs' => {excludeClientIPs},
-	""".format(excludeClientIPs=excludeClientIPs))
+        'excludeClientIPs' => {excludeClientIPs},
+        """.format(excludeClientIPs=excludeClientIPs))
 
-if excludeEntityIDs != '':
+    if excludeEntityIDs != '':
         print("""
-            'checkEntityID' => 'true',
-            'excludeEntityIDs' => {excludeEntityIDs},
-            'includeAttributes' => {includeAttributes},
-            'setPath' => '{setPath}',
-            'setKey' => '{setKey}',
-	""".format(excludeEntityIDs=excludeEntityIDs, includeAttributes=includeAttributes, setPath=setPath, setKey=setKey))
-            
-	print("),);")
+        'checkEntityID' => 'true',
+        'excludeEntityIDs' => {excludeEntityIDs},
+        'includeAttributes' => {includeAttributes},
+        'setPath' => '{setPath}',
+        'setKey' => '{setKey}',
+        """.format(excludeEntityIDs=excludeEntityIDs, includeAttributes=includeAttributes, setPath=setPath, setKey=setKey))
+    print("),);")
 @!@
