09f8e87467f5803a261dc27afc00c64467da9546
lukasmatusiewicz <lukas.matusiewicz@netknights.it>
2022-07-15 11:39:10 +0200

    Update 97saml20-idp-hosted.php

--- conffiles-v1.9/etc/simplesamlphp/metadata/97saml20-idp-hosted.php
+++ conffiles-v1.9/etc/simplesamlphp/metadata/97saml20-idp-hosted.php
@@ -4,11 +4,24 @@ domainname = configRegistry.get('domainname')
 entity_id = configRegistry.get('saml/idp/entityID',
     'https://{0!s}.{1!s}/simplesamlphp/saml2/idp/metadata.php'.format(hostname, domainname))
 
+// Here default values are defined
 url = configRegistry.get('privacyidea/saml/url', 'https://privacyidea')
 verifyhost = configRegistry.get('privacyidea/saml/verifyhost', 'true')
 verifypeer = configRegistry.get('privacyidea/saml/verifypeer', 'true')
 enabledPath = configRegistry.get('privacyidea/saml/enabledPath', 'privacyIDEA')
 enabledKey = configRegistry.get('privacyidea/saml/enabledKey', 'enabled')
+doTriggerChallenge = configRegistry.get('privacyidea/saml/doTriggerChallenge', 'false')
+serviceAccount = configRegistry.get('privacyidea/saml/serviceAccount', 'service')
+servicePass = configRegistry.get('privacyidea/saml/servicePass', 'service')
+doSendPassword = configRegistry.get('privacyidea/saml/doSendPassword', 'false')
+otpFieldHint = configRegistry.get('privacyidea/saml/otpFieldHint', 'Please enter OTP')
+passFieldHint = configRegistry.get('privacyidea/saml/passFieldHint', 'Please enter password')
+SSO = configRegistry.get('privacyidea/saml/SSO', 'false')
+preferredTokenType = configRegistry.get('privacyidea/saml/preferredTokenType', 'otp')
+doEnrollToken = configRegistry.get('privacyidea/saml/doEnrollToken', 'false')
+tokenType = configRegistry.get('privacyidea/saml/tokenType', '')
+tryFirstAuthentication = configRegistry.get('privacyidea/saml/tryFirstAuthentication', 'false')
+tryFirstAuthPass = configRegistry.get('privacyidea/saml/tryFirstAuthentication', '')
 
 excludeEntityIDs = configRegistry.get('privacyidea/saml/excludeEntityIDs', '')
 includeAttributes = configRegistry.get('privacyidea/saml/includeAttributes', '')
@@ -43,7 +56,7 @@ elif enabled == 'authproc':
         'doSendPassword' => '{doSendPassword}',
         'otpFieldHint' => '{otpFieldHint}',
         'passFieldHint' => '{passFieldHint}',
-        'SSO' => '{sso}',
+        'SSO' => '{SSO}',
         'preferredTokenType' => '{preferredTokenType}',
         'doEnrollToken' => '{doEnrollToken}',
         'tokenType' => '{tokenType}',
@@ -52,7 +65,7 @@ elif enabled == 'authproc':
 """.format(url=url, realm=realm, uidKey=uidKey, verifyhost=verifyhost.lower(), verifypeer=verifypeer.lower(),
             enabledPath=enabledPath, enabledKey=enabledKey, triggerChallenge=triggerChallenge.lower(),
             serviceAccount=serviceAccount, servicePass=servicePass, doSendPassword=doSendPassword,
-            otpFieldHint=otpFieldHint, passFieldHint=passFieldHint, sso=sso.lower(), preferredTokenType=preferredTokenType,
+            otpFieldHint=otpFieldHint, passFieldHint=passFieldHint, SSO=SSO.lower(), preferredTokenType=preferredTokenType,
             doEnrollToken=doEnrollToken, tokenType=tokenType, tryFirstAuthentication=tryFirstAuthentication.lower(),
             tryFirstAuthPass=tryFirstAuthPass))
 
