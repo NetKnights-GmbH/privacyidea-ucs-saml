6d2728ba87f665f4fce27ec40598b7974cff1abb
lukasmatusiewicz <lukas.matusiewicz@netknights.it>
2022-07-15 11:39:01 +0200

    Update 97authsources.php

--- conffiles-v1.9/etc/simplesamlphp/97authsources.php
+++ conffiles-v1.9/etc/simplesamlphp/97authsources.php
@@ -4,6 +4,14 @@ url = configRegistry.get('privacyidea/saml/url', 'https://privacyidea')
 realm = configRegistry.get('privacyidea/saml/realm', '')
 verifyhost = configRegistry.get('privacyidea/saml/verifyhost', 'true')
 verifypeer = configRegistry.get('privacyidea/saml/verifypeer', 'true')
+doTriggerChallenge = configRegistry.get('privacyidea/saml/doTriggerChallenge', 'false')
+serviceAccount = configRegistry.get('privacyidea/saml/serviceAccount', 'service')
+servicePass = configRegistry.get('privacyidea/saml/servicePass', 'service')
+doSendPassword = configRegistry.get('privacyidea/saml/doSendPassword', 'false')
+otpFieldHint = configRegistry.get('privacyidea/saml/otpFieldHint', 'Please enter OTP')
+passFieldHint = configRegistry.get('privacyidea/saml/passFieldHint', 'Please enter password')
+SSO = configRegistry.get('privacyidea/saml/SSO', 'false')
+preferredTokenType = configRegistry.get('privacyidea/saml/preferredTokenType', 'otp')
 print("""
 $config['privacyidea'] = array(
                 'privacyidea:PrivacyideaAuthSource',
